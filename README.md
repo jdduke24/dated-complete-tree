4f52b1c608bf4159ae9f9561177006234c0209d3
Jonathan D. Duke <j.duke24@imperial.ac.uk>
2025-03-21 12:47:48 +0000

    Minor documentation changes.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # dated-complete-tree
-Take the Open Tree and Life, resolve its topology, and interpolate dates for all nodes.
+Take the Open Tree of Life, resolve its topology, and interpolate dates for all nodes.
 
-This can be done repeatedly to generate a distribution of dated complete trees.
+Resolution can be performed repeatedly to generate a distribution of dated complete trees.
 
 Optionally, compute a distribution of evolutionary distinctiveness scores for each leaf node.
--- tree_dating.py
+++ tree_dating.py
@@ -3,13 +3,16 @@ import copy
 import logging
 logger = logging.getLogger(__name__)
 
-# mrad = most recent ancestor date of the current node - if the current node is longer ago than this, that's bad
+
 def remove_inconsistent_dates(parent, mrad):
+    """Recurse in preorder through the tree, removing the date info from any nodes that have dates older
+    than a date found on an ancestor. (mrad = most recent ancestor date)
+    """
     next_mrad = mrad
     # if we have a date, check it against the most recent ancestor
     if parent.date:
         if parent.date > mrad:
-            # if it is older than an ancestor, throw away all the date information at this node
+            # if it is older than an ancestor, throw away the date information at this node
             parent.date = None
             logger.info("Removing inconsistent date at node %s." % parent.name)
         else:
@@ -19,30 +22,35 @@ def remove_inconsistent_dates(parent, mrad):
         remove_inconsistent_dates(child, next_mrad)
 
 
-# label dates with nearest dates below them and path lengths (number of nodes) to those dates
-# return value is [oldest date found so far below this node, path length to it]
 def date_labelling(parent):
+    """Recurse in postorder through the tree, labelling each node with the found oldest date below it
+    and the path length (number of nodes) to that date.
+    Return value is: [oldest date found so far below this node, path length to it]
+    """
     if parent.is_leaf():
         if parent.date != 0:
             logger.warning("Leaf node %s has non-zero date." % parent.name)
-        oldest_path = [parent.date,0]
+        oldest_path = [parent.date, 0]
     else:
-        oldest_path = [0,0]
+        oldest_path = [0, 0]
         for child in parent.children:
-            oldest_path = max(oldest_path,date_labelling(child))
+            oldest_path = max(oldest_path, date_labelling(child))
 
     parent.add_feature("oldest_path", copy.deepcopy(oldest_path))
 
     if parent.date is None:
         oldest_path[1] += 1
     else:
-        oldest_path = [parent.date,1]
+        oldest_path = [parent.date, 1]
 
     return oldest_path
 
 
-# impute dates by dating each undated node between its parent and the oldest date found below it
 def impute_missing_dates(tre):
+    """Traverse the tree in preorder, giving each undated node a date spaced along the path between between
+    its parent (which always has a date, since this is preorder traversal) and the oldest date found below
+    it (as labelled by the date_labelling function). Assumes root node is dated.
+    """
     for node in tre.traverse(strategy="preorder"):
         if node.is_root():
             node.add_feature("imputed_date",False)
@@ -53,20 +61,10 @@ def impute_missing_dates(tre):
             node.imputed_date = True
 
 
-def compute_branch_lengths(tre, round_numbers=False):
-    for node in tre.traverse():
-        if node.up:
-            dist = node.up.date - node.date
-            if dist < 0:
-                logger.warning("Warning: Negative branch length above %s" % node.name)
-            if round_numbers:
-                node.dist = round_to_4sf(node.up.date - node.date)
-            else:
-                node.dist = node.up.date - node.date
-
-
 def round_to_4sf(x):
-    """Round to four sig figs: assumes input is less than 10000."""
+    """Round to four sig figs; intended for ages in Mya, so assumes input is less than 10000 (would be
+    older than the solar system).
+    """
     if x > 1000:
         return round(x)
     elif x > 100:
@@ -79,7 +77,22 @@ def round_to_4sf(x):
         return round(x,4)
 
 
+def compute_branch_lengths(tre, round_numbers=False):
+    """Fill in 'dist' field with branch lengths. Intended for a fully dated tree."""
+    for node in tre.traverse():
+        if node.up:
+            dist = node.up.date - node.date
+            if dist < 0:
+                logger.warning("Warning: Negative branch length above %s" % node.name)
+            if round_numbers:
+                node.dist = round_to_4sf(node.up.date - node.date)
+            else:
+                node.dist = node.up.date - node.date
+
+
 def write_tree_with_branch_lengths(tre, filename):
+    """Write out dated tree in Newick format (suitable for OneZoom). Branch lengths rounded
+    to 4 sig figs to save space in the text file."""
     compute_branch_lengths(tre, round_numbers=True)
 
     tre.write(outfile=filename,
